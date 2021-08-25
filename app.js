8db49d2c49c2726ff9766a99f89f1bfaeb8faa00
mouaz-m <maatouq.45@gmail.com>
2021-08-25 11:40:05 +0100

    deploying for the first time

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ mongoose.connect(process.env.DB_URL, {
   useCreateIndex: true,
   useFindAndModify: false
 });
-// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false , useCreateIndex: true});
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
